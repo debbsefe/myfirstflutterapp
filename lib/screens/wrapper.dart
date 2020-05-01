f1eedde2151a12c375de03f707b59b95d6849b8f
Mamus Eferha <eferhamamus@gmail.com>
2020-05-01 21:48:44 +0100

    initial commit

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,21 @@
+import 'package:first_app/models/user.dart';
+import 'package:first_app/screens/authenticate/authenticate.dart';
+import 'package:first_app/screens/home/home.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    final user = Provider.of<User>(context); 
+    
+    // return either Home of Authenticate widget
+    if(user == null){
+      return Authenticate();
+    }else{
+      return Home();
+    }
+  }
+}
---
+++ lib/shared/slideitem.dart
@@ -0,0 +1,29 @@
+// //import 'package:fitness_plus/models/slides.dart';
+// import 'package:flutter/material.dart';
+
+// class SlideItem extends StatelessWidget {
+//   final int index;
+//   SlideItem(this.index);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       crossAxisAlignment: CrossAxisAlignment.center,
+//     children: <Widget>[
+//       Container(
+//         width: 300,
+//         height: 300,
+//         child: new Image.asset(
+//             slideList[index].imageUrl,
+//             fit: BoxFit.contain,
+//         ),
+//       ),
+//       SizedBox(height: 10.0),
+//       slideList[index].description,
+//       SizedBox(height: 10.0),
+     
+//     ],
+//   );
+// }
+// }
